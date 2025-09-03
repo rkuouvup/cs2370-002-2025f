94a35dd9b6109bf7a92875cc6e31026dfb76e335
rkuouvup <rkuouvup@gmail.com>
2025-09-03 14:22:50 -0600

    Sep 3 in-class exercise

---
+++ cs2370-002-2025f/foreach.cpp
@@ -0,0 +1,30 @@
+// get a string input from the user
+// count how many uppercase letters are in the string
+// print the number
+// <cctype> library
+// isupper(character) ==> return true or false
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    int count = 0;
+    
+    cout << "Please enter a string: ";
+    getline(cin, s);
+    
+    //for (int i = 0; i < s.length(); i++) {
+    //    if (isupper(s.at(i)))
+    //        count++;
+    //}
+    
+    for (auto c : s) {  // for c in s
+        if (isupper(c))
+            count++;
+    }
+    
+    
+    cout << "There are " << count << " uppercase letters" << endl;
+    
+    return 0;
+}
--- cs2370-002-2025f/struct_prog3.cpp
+++ cs2370-002-2025f/struct_prog3.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    /*
-    for (int i = 0; i < 5; i++) {
-        cout << "Hello" << endl;
-    }*/
+/*int main() {
+    
+    //for (int i = 0; i < 5; i++) {
+    //    cout << "Hello" << endl;
+    //}
     int i = 0;
     while (i < 5) {
         cout << "Hello" << endl;
@@ -13,4 +13,4 @@ int main() {
     }
     
     return 0;
-}
+}*/
