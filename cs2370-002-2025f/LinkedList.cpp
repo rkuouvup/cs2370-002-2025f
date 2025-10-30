831a082bdafcc07fa11798051d3eeba88fa3c05f
rkuouvup <rkuouvup@gmail.com>
2025-10-30 08:26:24 -0600

    Oct 29 in-class exercise

--- cs2370-002-2025f/LinkedList.cpp
+++ cs2370-002-2025f/LinkedList.cpp
@@ -13,10 +13,63 @@ private:
     LinkedListNode *head;
 public:
     LinkedList() : head(nullptr) {}
+    LinkedList(const LinkedList &obj);
     ~LinkedList();
+    LinkedListNode* GetHead() {return head;}
+    LinkedList& operator=(const LinkedList &obj);
     void Prepend(int data);
     void Print();
 };
+
+LinkedList::LinkedList(const LinkedList &obj) {
+    if (obj.head == nullptr) {
+        this->head = nullptr;
+    } else {
+        head = new LinkedListNode(obj.head->data);
+        LinkedListNode *current = head;
+        LinkedListNode *currentObj = obj.head;
+        
+        while (currentObj->next != nullptr) {
+            current->next = new LinkedListNode(currentObj->next->data);
+            currentObj = currentObj->next;
+            current = current->next;
+        }
+    }
+}
+
+LinkedList& LinkedList::operator=(const LinkedList &obj) {
+    if (this != &obj) { // prevent self-assign
+        // delete old list
+        if (head != nullptr) {
+            LinkedListNode *current = head;
+            LinkedListNode *next;
+            
+            while(current != nullptr) {
+                next = current->next;
+                delete current;
+                current = next;
+            }
+        }
+        
+        // copy list from the source
+        if (obj.head == nullptr) {
+            this->head = nullptr;
+        } else {
+            head = new LinkedListNode(obj.head->data);
+            LinkedListNode *current = head;
+            LinkedListNode *currentObj = obj.head;
+            
+            while (currentObj->next != nullptr) {
+                current->next = new LinkedListNode(currentObj->next->data);
+                currentObj = currentObj->next;
+                current = current->next;
+            }
+        }
+        
+    }
+    return *this;
+    
+}
 
 LinkedList::~LinkedList() {
     LinkedListNode *next = nullptr;
@@ -46,13 +99,51 @@ void LinkedList::Print() {
 }
 
 
-/*int main() {
+int main() {
+    //LinkedListNode n1(1);
+    //LinkedListNode n2(n1);
+    
+    //cout << "n1: " << n1.GetData() << endl;
+    //cout << "n2: " << n2.GetData() << endl;
+    
+    //n2.SetData(10);
+    
+    //cout << "n1: " << n1.GetData() << endl;
+    //cout << "n2: " << n2.GetData() << endl;
+    
+    // Copy Constructor Demonsstration
+    //LinkedList list;
+    //list.Prepend(1); list.Prepend(2); list.Prepend(3); list.Prepend(4);
+    
+    //LinkedList l2(list);
+    
+    //cout << "l1: "; list.Print();
+    //cout << "l2: "; l2.Print();
+    
+    //(l2.GetHead())->SetData(40);
+    // (*(l2.GetHead())).SetData(40);
+    
+    //cout << "l1: "; list.Print();
+    //cout << "l2: "; l2.Print();
+    
+    // Copy Assignment Operator Demonstration
     LinkedList list;
-    list.Prepend(1);
-    list.Prepend(2);
-    list.Prepend(3);
-    list.Prepend(4);
-    list.Print();
-    list.Print();
+    list.Prepend(1); list.Prepend(2); list.Prepend(3); list.Prepend(4);
+    LinkedList l2;
+    l2.Prepend(100);
+    
+    l2 = list;
+    
+    cout << "l1: "; list.Print();
+    cout << "l2: "; l2.Print();
+    
+    (l2.GetHead())->SetData(40);
+    
+    cout << "l1: "; list.Print();
+    cout << "l2: "; l2.Print();
+    
+    
+    
+    
     return 0;
-}*/
+}
